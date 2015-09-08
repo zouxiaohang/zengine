c3ecced2f6f3a708fed3662f0d9c4d0930ae1a42
zouxiaohang <1210603696@qq.com>
2015-09-08 14:57:19 +0800

    ci

--- zengine/src/math/math_util.hpp
+++ zengine/src/math/math_util.hpp
@@ -1,17 +1,30 @@
 #ifndef _MATH_UTIL_H_
 #define _MATH_UTIL_H_
 
+
+#define _USE_MATH_DEFINES
 #include <math.h>
 
 namespace zengine
 {
 #define EPSILON 0.0001f
+#define PI M_PI
 
-	bool floatEqual(float f1, float f2)
+	inline bool floatEqual(float f1, float f2)
 	{
 		if (fabs(f1 - f2) < EPSILON)
 			return true;
 		return false;
+	}
+
+	inline float angleToRadian(float angle)
+	{
+		return angle * PI / 180.0f;
+	}
+
+	inline float radianToAngle(float radian)
+	{
+		return 180.0f * radian / PI;
 	}
 }
 
